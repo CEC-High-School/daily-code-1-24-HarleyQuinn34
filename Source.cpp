c4c6ba8118bca0caf122966e05998cf666978bf2
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-01-24 13:16:50 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	string track [8] = { "When Thugz Cry-Tupac", "j-cole - Change", "Until We are Rich- Ice Cube", "Today was a good day-Ice Cube", "song", "son1", "song2" };
+	
+	for (int i = 0; i < 8; i++) {
+		
+			cout << track[i]<<endl;
+	}
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+}
